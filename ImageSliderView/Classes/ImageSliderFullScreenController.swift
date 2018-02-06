d9a0b030dff9bc9817503f9cede9483f7684439a
Salahuddin Khan <salahuddin@codetoart.com>
2018-02-06 13:07:15 +0530

    Converted 2 files to swift 3

--- ImageSliderView/Classes/ImageSliderFullScreenController.swift
+++ ImageSliderView/Classes/ImageSliderFullScreenController.swift
@@ -12,15 +12,15 @@ import SnapKit
 class ImageSliderFullScreenController: UIViewController {
     
     var images: Array<NSURL>?
-    var tintColor: UIColor = UIColor.blueColor()
-    var font: UIFont = UIFont.systemFontOfSize(15)
+    var tintColor: UIColor = UIColor.blue
+    var font: UIFont = UIFont.systemFont(ofSize: 15)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.blackColor()
-        let imageSliderView = ImageSliderView(frame: CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds)))
+        self.view.backgroundColor = UIColor.black
+        let imageSliderView = ImageSliderView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height))
         imageSliderView.allowFullscreenOnTap = false
         imageSliderView.dataSource = self
         self.view.addSubview(imageSliderView)
@@ -30,7 +30,7 @@ class ImageSliderFullScreenController: UIViewController {
         imageSliderView.reloadData()
         
         let closeBtn = UIButton()
-        closeBtn.setTitle(NSLocalizedString("Close", comment: ""), forState: .Normal)
+        closeBtn.setTitle(NSLocalizedString("Close", comment: ""), for: .normal)
         closeBtn.backgroundColor = tintColor
         closeBtn.titleLabel?.font = self.font
         self.view.addSubview(closeBtn)
@@ -44,8 +44,8 @@ class ImageSliderFullScreenController: UIViewController {
         closeBtn.layer.cornerRadius = 5
         closeBtn.clipsToBounds = true
         closeBtn.layer.borderWidth = 1.0
-        closeBtn.layer.borderColor = UIColor.whiteColor().CGColor
-        closeBtn.addTarget(self, action: #selector(closeBtnTapped), forControlEvents: .TouchUpInside)
+        closeBtn.layer.borderColor = UIColor.white.cgColor
+        closeBtn.addTarget(self, action: #selector(closeBtnTapped), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,12 +53,12 @@ class ImageSliderFullScreenController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
+//    func prefersStatusBarHidden() -> Bool {
+//        return true
+//    }
     
     func closeBtnTapped() {
-        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
     }
 
 }
